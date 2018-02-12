ca48603d70054e157efd96ef09a977fd60743cbd
weichao66666 <weichao66666@gmail.com>
2018-02-12 17:12:17 +0800

    添加 AutoLayout 适配方式。

--- app/src/main/java/io/weichao/percentlayoutcomparisondemo/MainActivity.java
+++ app/src/main/java/io/weichao/percentlayoutcomparisondemo/MainActivity.java
@@ -7,6 +7,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_constraintlayout);
+        setContentView(R.layout.activity_main_autolayout);
     }
 }
